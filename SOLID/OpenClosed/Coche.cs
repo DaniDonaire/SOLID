10df11636c6173ff714fc9a3876d329227054708
danidonaire <dani.g.donaire@gmail.com>
2019-02-25 22:20:15 +0100

    Open Close From SOLID Done. Close #2

---
+++ SOLID/OpenClosed/Coche.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenClosed
+{
+    public class Coche
+    {
+        public string Name { get; set; }
+        public SubType Type { get; set; }
+        public Model Modelo { get; set; }
+    }
+
+    public class CochesFilter
+    {
+        public List<Coche> FilterByType(IEnumerable<Coche> coches, SubType type) =>
+                coches.Where(m => m.Type == type).ToList();
+    }
+}
---
+++ SOLID/OpenClosed/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenClosed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var coches = new List<Coche>
+            {
+                new Coche { Name = "Ibiza normal", Modelo = Model.Ibiza, Type = SubType.Normal },
+                new Coche { Name = "Ibiza FR", Modelo = Model.Ibiza, Type = SubType.Fr },
+                new Coche { Name = "Leon FR", Modelo = Model.Leon, Type = SubType.Fr },
+                new Coche { Name = "Ibiza Coupra", Modelo = Model.Ibiza, Type = SubType.Coupra },
+                new Coche { Name = "Leon Coupra", Modelo = Model.Leon, Type = SubType.Coupra }
+            };
+
+            var filter = new CochesFilter();
+
+            var cochesFr = filter.FilterByType(coches, SubType.Fr);
+            Console.WriteLine("All Cars Fr");
+            foreach (var coche in cochesFr)
+            {
+                Console.WriteLine($"Name: {coche.Name}, Type: {coche.Type}, Screen: {coche.Modelo}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    public enum SubType
+    {
+        Normal,
+        Fr,
+        Coupra
+    }
+    public enum Model
+    {
+        Ibiza,
+        Leon
+    }
+    
+
+}
