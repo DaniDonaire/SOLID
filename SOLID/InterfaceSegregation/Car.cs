09cf9bfb02107f2705cb3a021228514586b6f41d
danidonaire <dani.g.donaire@gmail.com>
2019-02-25 22:39:18 +0100

    Interface segregation inplemented. Close #4

---
+++ SOLID/InterfaceSegregation/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceSegregation
+{
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            //actions to drive a car
+            Console.WriteLine("Driving a car");
+        }
+    }
+}
---
+++ SOLID/InterfaceSegregation/IMultiFunctionalVehicle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceSegregation
+{
+    public interface IMultiFunctionalVehicle : ICar, IAirplane
+    {
+    }
+}
